ef108558f334f90ca311beadc5e4fd08a5008f54
SelviP2011 <59732368+SelviP2011@users.noreply.github.com>
2020-09-06 16:21:09 +0530

    Create Eqnsolver_wid_2var.py

---
+++ Eqnsolver_wid_2var.py
@@ -0,0 +1,21 @@
+#Eqn solver with two unknown variables
+def xy(a):
+    h=""
+    for i in a:
+        if i=="x":
+            if(h in "+-"):    h+="1"
+            t=int(h);h=""
+        elif i=="y":
+            if(h in "+-"):    h+="1"
+            g=int(h);h=""
+        else:
+            h+=i
+    y=int(h[1:])
+    return t,g,(-1*y)
+a=str(input().strip())
+b=str(input().strip())
+c,d,e=xy(a);x,y,z=xy(b)
+print(int(((d*z)-(y*e))/((c*y)-(x*d))))
+print(int(((e*x)-(z*c))/((c*y)-(x*d))))
+
+    
